096a8089a00a62d3edfa82d1c1cedd81e9df0ad0
Solovova <vsolo0@gmail.com>
2021-01-17 19:22:27 +0200

    07

--- src/App.js
+++ src/App.js
@@ -1,43 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header";
 
 function App1() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit1 <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit1 <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 const App = () => {
-  return (
-      <div className="App">
-          <ul>
-              <li>
-                  Test1
-              </li>
-              <li>
-                  Test2
-              </li>
-              <li>
-                  Test3
-              </li>
-          </ul>
-        Test
-      </div>);
+    return (
+        <div className="App">
+            <Header/>
+            <ul>
+                <li>
+                    Test1
+                </li>
+                <li>
+                    Test2
+                </li>
+                <li>
+                    Test3
+                </li>
+            </ul>
+            Test
+        </div>
+
+    );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <div>
+            <a href="#s">Link #1</a>
+            <a href="#s">Link #2</a>
+            <a href="#s">Link #3</a>
+        </div>
+    )
+}
+
+export default Header;
