c9a3b9e3c4aa13d29f8cf3b2a8d10cb3a3118b8c
Solovova <vsolo0@gmail.com>
2021-01-17 20:26:44 +0200

    11

--- src/App.js
+++ src/App.js
@@ -1,17 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
-import Header from "./components/Header";
-import Technologies from "./components/Technologies";
-import Footer from "./components/Footer";
-
-
 
 const App = () => {
     return (
-        <div className="App">
-            <Header/>
-            <Technologies />
-            <Footer />
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img src='https://retailandhospitalityhub.com/wp-content/uploads/2018/11/img.jpg'/>
+            </header>
+            <nav className='nav'>
+                <div>
+                    <a>
+                        Profile
+                    </a>
+                </div>
+                <div>
+                    <a>
+                        Messages
+                    </a>
+                </div>
+                <div>
+                    <a>
+                        News
+                    </a>
+                </div>
+                <div>
+                    <a>
+                        Music
+                    </a>
+                </div>
+                <div></div>
+                <div>
+                    <a>
+                        Settings
+                    </a>
+                </div>
+            </nav>
+
+            <div className='content'>
+                <div>
+                    Ava + descr
+                </div>
+                <div>
+                    New posts
+                </div>
+                <div>
+                    My posts
+                    <div>
+                        post1
+                    </div>
+                    <div>
+                        post2
+                    </div>
+                </div>
+
+            </div>
+
         </div>
     );
 }
