73df9f23cead3b95d508be9a2c950fbc4393c17c
Solovova <vsolo0@gmail.com>
2021-01-18 15:51:03 +0200

    23.01

--- src/components/Content/Profile/Profile.jsx
+++ src/components/Content/Profile/Profile.jsx
@@ -1,15 +1,10 @@
-import './Profile.module.css'
 import s from './Profile.module.css'
 import Posts from "./Posts/Posts";
+import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
 const Profile = () => {
     return <div className={s.content}>
-        <div>
-            <img src='https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350' />
-        </div>
-        <div>
-            ava + description
-        </div>
+        <ProfileInfo/>
         <Posts />
 
     </div>
