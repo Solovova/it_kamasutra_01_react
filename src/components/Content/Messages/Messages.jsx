a08e4d4bdd71ecbafc792fba8f47973b7a7a346a
Solovova <vsolo0@gmail.com>
2021-01-18 11:43:48 +0200

    21.00

--- src/components/Content/Messages/Messages.jsx
+++ src/components/Content/Messages/Messages.jsx
@@ -1,11 +1,25 @@
 import s from './Messages.module.css'
 
 const Messages = () => {
+    console.log(s)
+    console.log("asa")
     return (
-        <div className={s.dialog}>
-            Dialog
+        <div className={s.messagesMain}>
+            <div className={s.dialogs}>
+                <div className={s.dialogsItems}> Solo  </div>
+                <div className={`${s.dialogsItems} ${s.active}`}>Ira</div>
+            </div>
+
+            <div className={s.messages}>
+                <div className={s.message}> Message 1 </div>
+                <div className={s.message}> Message 2 </div>
+                <div className={s.message}> Message 3 </div>
+                <div className={s.message}> Message 4 </div>
+                <div className={s.message}> Message 5 </div>
+                <div className={s.message}> Message 7 </div>
+            </div>
         </div>
     );
-}
+};
 
 export default Messages;
