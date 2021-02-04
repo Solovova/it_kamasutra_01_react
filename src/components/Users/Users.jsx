e8f0d3b415a9739e25af1d5a4027a778320f08b7
Solovova <vsolo0@gmail.com>
2021-02-04 12:47:33 +0200

    51.00

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,42 +1,13 @@
 import styles from './Users.module.css'
+import * as axios from 'axios';
+import userPhoto from '../../assets/images/user.png';
 
 const Users = (props) => {
     if (props.users.length === 0) {
-        props.usersSet ( [
-            {
-                id: 1,
-                photoUrl:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_l0NXJRVwDgPOEw_GsSjXrmGo3hcd_iIzLQ&usqp=CAU',
-                followed: false,
-                fullName: 'Solo',
-                status:'worker',
-                location: {
-                    city: 'Minsk',
-                    country: 'Belarus'
-                }
-            },
-            {
-                id: 2,
-                photoUrl:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_l0NXJRVwDgPOEw_GsSjXrmGo3hcd_iIzLQ&usqp=CAU',
-                followed: true,
-                fullName: 'Solo 2',
-                status:'boss',
-                location: {
-                    city: 'Minsk 2',
-                    country: 'Belarus 2'
-                }
-            },
-            {
-                id: 3,
-                photoUrl:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_l0NXJRVwDgPOEw_GsSjXrmGo3hcd_iIzLQ&usqp=CAU',
-                followed: false,
-                fullName: 'Solo 3',
-                status:'student',
-                location: {
-                    city: 'Minsk 3',
-                    country: 'Belarus 3'
-                }
-            }
-        ])
+        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+            .then(response => {
+                props.usersSet(response.data.items);
+            });
     }
 
 
@@ -48,7 +19,7 @@ const Users = (props) => {
                         <div key={user.id}>
                             <span>
                                 <div>
-                                    <img alt="user" src={user.photoUrl} className={styles.userPhoto}/>
+                                    <img src={user.photos.small != null ? user.photos.small : userPhoto} className={styles.userPhoto}/>
                                 </div>
                                 <div>
                                     {user.followed
@@ -60,12 +31,8 @@ const Users = (props) => {
                             </span>
                             <span>
                                 <span>
-                                    <div>{user.fullName}</div>
+                                    <div>{user.name}</div>
                                     <div>{user.status}</div>
-                                </span>
-                                <span>
-                                    <div>{user.location.city}</div>
-                                    <div>{user.location.country}</div>
                                 </span>
                             </span>
                         </div>
